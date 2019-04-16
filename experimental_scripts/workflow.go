ea254dc0841cff52eb432fb4f2edebfa70f303cd
Samuel Lampa <samuel.lampa@gmail.com>
2019-04-17 01:19:32 +0200

    Rename python script

--- experimental_scripts/workflow.go
+++ experimental_scripts/workflow.go
@@ -18,7 +18,7 @@ func main() {
 	targetDirGlobber.InDependency().From(createTargetDatasets.Out("doneflag"))
 
 	// Balance the target datasets
-	balanceTargetDatasets := wf.NewProc("balance_dataset", "python3 ../balancing_targets.py {i:unbalanced} {o:balanced}; echo 'done' > {o:doneflag}")
+	balanceTargetDatasets := wf.NewProc("balance_dataset", "python3 ../balance_dataset.py {i:unbalanced} {o:balanced}; echo 'done' > {o:doneflag}")
 	balanceTargetDatasets.In("unbalanced").From(targetDirGlobber.Out())
 	balanceTargetDatasets.SetOut("balanced", "data/balanced_datasets/$(basename {i:unbalanced|%.tsv}).balanced.tsv")
 	balanceTargetDatasets.SetOut("doneflag", "data/balanced_datasets/$(basename {i:unbalanced|%.tsv}).done")
