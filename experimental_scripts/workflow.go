16fbb9dbbaa20332b7124b2d7d3323101613bf56
Samuel Lampa <samuel.lampa@gmail.com>
2019-04-17 01:18:18 +0200

    Make balance_datasets.py take outfile as explicit filename

--- experimental_scripts/workflow.go
+++ experimental_scripts/workflow.go
@@ -14,14 +14,14 @@ func main() {
 	createTargetDatasets.SetOut("doneflag", "data/target_datasets.done")
 
 	// Glob the target dataset files into a stream
-	targetDirGlobber := spc.NewFileGlobberDependent(wf, "glob_target_datasets", "./data/target_datasets/[^.]*.json")
+	targetDirGlobber := spc.NewFileGlobberDependent(wf, "glob_target_datasets", "./data/target_datasets/[^.]*.tsv")
 	targetDirGlobber.InDependency().From(createTargetDatasets.Out("doneflag"))
 
 	// Balance the target datasets
-	balanceTargetDatasets := wf.NewProc("balance_dataset", "python3 ../balancing_targets.py {i:inpfiles} {o:balanced_targets_dir}; echo 'done' > {o:doneflag}")
-	balanceTargetDatasets.In("inpfiles").From(targetDirGlobber.Out())
-	balanceTargetDatasets.SetOut("balanced_targets_dir", "data/balanced_target_datasets")
-	balanceTargetDatasets.SetOut("doneflag", "{i:inpfiles|%.json}.done")
+	balanceTargetDatasets := wf.NewProc("balance_dataset", "python3 ../balancing_targets.py {i:unbalanced} {o:balanced}; echo 'done' > {o:doneflag}")
+	balanceTargetDatasets.In("unbalanced").From(targetDirGlobber.Out())
+	balanceTargetDatasets.SetOut("balanced", "data/balanced_datasets/$(basename {i:unbalanced|%.tsv}).balanced.tsv")
+	balanceTargetDatasets.SetOut("doneflag", "data/balanced_datasets/$(basename {i:unbalanced|%.tsv}).done")
 
 	wf.RunTo("balance_dataset")
 }
