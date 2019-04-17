5bb117de88903bef1759dc8ac4b01d439b034843
Samuel Lampa <samuel.lampa@gmail.com>
2019-04-17 03:06:28 +0200

    Use new 'basename' path format modifier to format filename of balanced dataset

--- experimental_scripts/workflow.go
+++ experimental_scripts/workflow.go
@@ -18,10 +18,10 @@ func main() {
 	targetDirGlobber.InDependency().From(createTargetDatasets.Out("doneflag"))
 
 	// Balance the target datasets
-	balanceTargetDatasets := wf.NewProc("balance_dataset", "python3 ../balance_dataset.py {i:unbalanced} {o:balanced}; echo 'done' > {o:doneflag}")
+	balanceTargetDatasets := wf.NewProc("balance_dataset", "python3 ../balance_dataset.py --dbfile=../database --infile={i:unbalanced} --outfile={o:balanced}; echo 'done' > {o:doneflag}")
 	balanceTargetDatasets.In("unbalanced").From(targetDirGlobber.Out())
-	balanceTargetDatasets.SetOut("balanced", "data/balanced_datasets/$(basename {i:unbalanced|%.tsv}).balanced.tsv")
-	balanceTargetDatasets.SetOut("doneflag", "data/balanced_datasets/$(basename {i:unbalanced|%.tsv}).done")
+	balanceTargetDatasets.SetOut("balanced", `data/balanced_datasets/{i:unbalanced|basename|%.tsv}.balanced.tsv`)
+	balanceTargetDatasets.SetOut("doneflag", "{o:balanced}.done")
 
-	wf.RunTo("balance_dataset")
+	wf.Run()
 }
